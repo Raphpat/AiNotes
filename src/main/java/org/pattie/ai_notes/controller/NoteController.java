0fdaaeb5ac8b39fed2f91132d1a893866b00943c
Raphael Pattie <rpattiecsi@gmail.com>
2025-05-27 14:38:27 +0200

    feat(ain-7): proper layout

--- src/main/java/org/pattie/ai_notes/controller/NoteController.java
+++ src/main/java/org/pattie/ai_notes/controller/NoteController.java
@@ -6,21 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/notes")
+@CrossOrigin
 public class NoteController {
     Logger log = LoggerFactory.getLogger(NoteController.class);
 
